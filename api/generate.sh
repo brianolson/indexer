a0f0a8a1dfecd9cbe5f38d9ce1504f501d51116e
Will Winder <wwinder.unh@gmail.com>
2020-05-04 13:22:41 -0400

    Improve generate script + indexer.oas2.json
    Add top-level produces field to fix the problem sed was working aorund.
    Use go get to install oapi-codegen in the generate script
    Minor cleanup of openapi metadata

--- api/generate.sh
+++ api/generate.sh
@@ -4,11 +4,17 @@ set -e
 rootdir=`dirname $0`
 pushd $rootdir
 
+pushd ${rootdir}/../
+  $(GO111MODULE=on go get -u "github.com/algorand/oapi-codegen/...@v1.3.5-algorand4")
+popd
+
+
 # Convert v2 to v3
 curl -s -X POST "https://converter.swagger.io/api/convert" -H "accept: application/json" -H "Content-Type: application/json" -d @./indexer.oas2.json  -o 3.json
-cat 3.json | json_pp > indexer.oas3.yml
+
+# Sort keys, format json and rename 3.json -> algod.oas3.yml
+python3 -c "import json; import sys; json.dump(json.load(sys.stdin), sys.stdout, indent=2, sort_keys=True)" < 3.json > indexer.oas3.yml
 rm 3.json
-sed -i 's/\*.\*/application\/json/g' indexer.oas3.yml
 
 echo "generating code."
 oapi-codegen -package generated -type-mappings integer=uint64 -generate types -o generated/types.go indexer.oas3.yml
--- go.mod
+++ go.mod
@@ -6,11 +6,15 @@ require (
 	github.com/algorand/go-algorand-sdk v1.2.2-0.20200225152129-2f6afbfaba16
 	github.com/algorand/go-codec/codec v1.1.7
 	github.com/algorand/oapi-codegen v1.3.5-algorand4
-	github.com/getkin/kin-openapi v0.3.1
+	github.com/getkin/kin-openapi v0.8.0
 	github.com/labstack/echo/v4 v4.1.16
 	github.com/lib/pq v1.3.0
 	github.com/sirupsen/logrus v1.5.0
 	github.com/spf13/cobra v0.0.5
 	github.com/stretchr/objx v0.1.1 // indirect
-	github.com/stretchr/testify v1.4.0
+	github.com/stretchr/testify v1.5.1
+	golang.org/x/crypto v0.0.0-20200429183012-4b2356b1ed79 // indirect
+	golang.org/x/net v0.0.0-20200501053045-e0ff5e5a1de5 // indirect
+	golang.org/x/sys v0.0.0-20200501145240-bc7a7d42d5c3 // indirect
+	golang.org/x/tools v0.0.0-20200504152539-33427f1b0364 // indirect
 )
